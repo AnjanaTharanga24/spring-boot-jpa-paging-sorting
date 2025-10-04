7885b55d461cc344dc24d1d77fb35762ff809bac
AnjanaTharanga24 <it21826436@my.sliit.lk>
2025-10-04 11:01:43 +0530

    new changes on model mapper

--- src/main/java/com/example/demo/config/ModelMapperConfig.java
+++ src/main/java/com/example/demo/config/ModelMapperConfig.java
@@ -1,7 +1,6 @@
 package com.example.demo.config;
 
 import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +9,6 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.STRICT)
-                .setFieldMatchingEnabled(true);
-
-        return modelMapper;
+        return new ModelMapper();
     }
 }
